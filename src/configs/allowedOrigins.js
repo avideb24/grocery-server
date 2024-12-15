d0fe39c27ef409fbd84dddf3da04990d24e43810
avideb24 <debavi24@gmail.com>
2024-12-15 11:42:21 +0600

    added

--- src/configs/allowedOrigins.js
+++ src/configs/allowedOrigins.js
@@ -1,4 +1,4 @@
-const allowedOrigins = ["http://localhost:4000", "http://localhost:3000"];
+const allowedOrigins = ["http://localhost:4000", "http://localhost:3000", "https://grocery-client-coral.vercel.app/"];
 
 module.exports = {
   allowedOrigins,
