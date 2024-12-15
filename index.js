8332845d1992ca6458c4d6b1c9d5ca20efa61a1e
avideb24 <debavi24@gmail.com>
2024-12-15 09:53:53 +0600

    here now cooo

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const app = require("./app");
-const { PORT } = require("./src/configs/config");
+// const app = require("./app");
+// const { PORT } = require("./src/configs/config");
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+// index.js
+const app = require("./app");
+
+// Export the app for Vercel to handle the routing
+module.exports = app;
+
